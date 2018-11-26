14bb6cee8eec37e7225abf58a94c281a5c1fcf6c
Thegamerboy1001 <thegamerboy7@gmail.com>
2018-11-26 14:55:35 -0700

    Removed hello world

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <cashRegister></cashRegister>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import cashRegister from './components/cashRegister.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    cashRegister
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/cashRegister.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1>Bleh</h1>
+  </div>
+</template>
+
+<script>
+  
+  export default {
+      data () {
+          return {
+              storeItems: [
+                  
+              ],
+              cartItems: []
+          }
+      }
+  }
+  
+</script>
+
+<style>
+.container {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
